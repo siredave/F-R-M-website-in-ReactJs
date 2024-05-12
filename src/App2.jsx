f4668b520d633fdf8325473731dba938a14c5dd5
siredave <siredave1@gmail.com>
2024-05-12 16:39:07 +0100

    Update

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from "react";
+import './App.css';
 
-import { Blog, Features, Footer, Header, Possibility, Whatgpt3 } from  './containers'
-import { Brand, Navbar, Cta } from  './components'
+
+import {
+	Blog,
+	Features,
+	Footer,
+	Header,
+	Possibility,
+	Whatgpt3,
+} from "./containers";
+import { Brand, Navbar, Cta } from "./components";
 
 export default function App2() {
-  return (
-    <>
-        <div>
-        <div>
-            <Navbar/>
-            <Header/>
-        </div>
-        <Brand/>
-        <Whatgpt3/>
-        <Features/>
-        <Possibility/>
-        <Cta/>
-        <Blog/>
-        <Footer/>
-    </div>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<div>
+					<Navbar />
+					<Header />
+				</div>
+				<Brand />
+				<Whatgpt3 />
+				<Features />
+				<Possibility />
+				<Cta />
+				<Blog />
+				<Footer />
+			</div>
+		</>
+	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import App2 from './App2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App />b  */}
+    <App2/>
   </React.StrictMode>,
 )
